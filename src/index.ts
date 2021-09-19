082afa2be43d3fce192f266d8b2a38932e264fb0
Hidoni <ron30086@walla.com>
2021-09-20 00:19:45 +0300

    Fixed bot initialization in index.js

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import * as log4js from 'log4js';
 import { Intents } from 'discord.js';
 import { Bot } from './client/Client';
+import * as Path from 'path';
 
 log4js.configure('./config/log4js.json');
 const logger = log4js.getLogger('starboard');
@@ -13,11 +14,17 @@ for (const envVar of REQUIRED_ENV_VARS) {
     }
 }
 
-const bot = new Bot({
-    intents: [Intents.FLAGS.GUILDS],
-    token: process.env.BOT_TOKEN!,
-    appId: process.env.BOT_APPLICATION_ID!,
-    debugGuildId: process.env.DEBUG_GUILD_ID,
-});
+const bot = new Bot(
+    {
+        intents: [Intents.FLAGS.GUILDS],
+        token: process.env.BOT_TOKEN!,
+        appId: process.env.BOT_APPLICATION_ID!,
+        debugGuildId: process.env.DEBUG_GUILD_ID,
+        commandsFolder: Path.join(__dirname, 'commands/'),
+        eventsFolder: Path.join(__dirname, 'events/'),
+    },
+    logger
+);
 
+logger.info("Starting bot");
 bot.run();
