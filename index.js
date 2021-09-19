171bb00b9b68d2edc4e3d0c03ac6a2e404596f8e
Hidoni <ron30086@walla.com>
2021-09-19 15:13:28 +0300

    Added basic logging for the bot

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 require('dotenv').config();
-
+const log4js = require('log4js');
+const path = require('path');
 const { Client, Intents } = require('discord.js');
+
+log4js.configure('./config/log4js.json');
+const logger = log4js.getLogger(path.relative(process.cwd(), __filename));
+
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 client.once('ready', () => {
-	console.log('Ready!');
+    logger.info(`Bot ready, logged in as ${client.user.username}#${client.user.discriminator}`);
 });
 
 client.login(process.env.BOT_TOKEN);
