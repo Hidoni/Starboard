ffea039b3e48d2e27a74b0207b5743f311037c16
Hidoni <ron30086@walla.com>
2021-09-20 20:14:07 +0300

    Added Server Config table model

---
+++ src/database/models/ServerConfig.ts
@@ -0,0 +1,41 @@
+import { Sequelize } from 'sequelize/types';
+import { DataTypes } from 'sequelize';
+import { DatabaseModelInitializer } from '../../interfaces/DatabaseModel';
+
+export const initializer: DatabaseModelInitializer = (sequelize: Sequelize) => {
+    return sequelize.define(
+        'server_config',
+        {
+            guild_id: {
+                type: DataTypes.STRING,
+                primaryKey: true,
+            },
+            sfw_channel_id: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            nsfw_channel_id: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            emoji: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                defaultValue: '\u2b50',
+            },
+            is_unicode: {
+                type: DataTypes.BOOLEAN,
+                allowNull: false,
+                defaultValue: true,
+            },
+            minimum_reactions: {
+                type: DataTypes.INTEGER,
+                allowNull: false,
+                defaultValue: 5,
+            },
+        },
+        {
+            timestamps: false,
+        }
+    );
+};
