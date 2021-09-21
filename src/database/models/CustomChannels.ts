66e299451aece5209b7bef166e38193e7f1add0a
Hidoni <ron30086@walla.com>
2021-09-21 20:14:35 +0300

    Added guildId to CustomChannels.ts

--- src/database/models/CustomChannels.ts
+++ src/database/models/CustomChannels.ts
@@ -3,21 +3,29 @@ import { DataTypes } from 'sequelize';
 import { DatabaseModelInitializer } from '../../interfaces/DatabaseModel';
 import { CustomChannelInstance } from '../../interfaces/CustomChannels';
 
-export const initialize: DatabaseModelInitializer<CustomChannelInstance> = (sequelize: Sequelize) => {
+export const initialize: DatabaseModelInitializer<CustomChannelInstance> = (
+    sequelize: Sequelize
+) => {
     return sequelize.define(
         'custom_starboard_channels',
         {
             channelId: {
                 type: DataTypes.STRING,
+                allowNull: false,
                 primaryKey: true,
             },
             starboardId: {
                 type: DataTypes.STRING,
                 allowNull: false,
             },
+            guildId: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
         },
         {
             timestamps: false,
+            indexes: [{fields: ['guildId']}]
         }
     );
 };
