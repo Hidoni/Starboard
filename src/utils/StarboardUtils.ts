8c7d20c5679f1e7bc207c445c1e1ed4fbfffd2e3
Hidoni <ron30086@walla.com>
2021-09-21 22:24:43 +0300

    Added function to generate a starboard embed

--- src/utils/StarboardUtils.ts
+++ src/utils/StarboardUtils.ts
@@ -1,11 +1,69 @@
-import { Message, Snowflake, TextChannel } from 'discord.js';
+import {
+    Message,
+    MessageEmbed,
+    MessageReaction,
+    Snowflake,
+    TextChannel,
+} from 'discord.js';
 import Database from '../database/DatabaseObject';
 import { GuildConfigInstance } from '../interfaces/GuildConfig';
 
-async function findStarboardChannelForTextChannel(config: GuildConfigInstance, channel: TextChannel, database: Database): Promise<Snowflake> {
+const STARBOARD_EMBED_COLOR: readonly [number, number, number] = [255, 172, 51];
+const DEFAULT_STARBOARD_EMOJI: string = '⭐';
+
+export async function findStarboardChannelForTextChannel(
+    config: GuildConfigInstance,
+    channel: TextChannel,
+    database: Database
+): Promise<Snowflake> {
     const customChannel = await database.getCustomChannel(channel.id);
     if (customChannel) {
         return customChannel.starboardId;
     }
     return channel.nsfw ? config.nsfwChannelId : config.sfwChannelId;
+}
+
+export async function generateStarboardEmbed(
+    reaction: MessageReaction
+): Promise<MessageEmbed> {
+    const message = await reaction.message.fetch();
+    let embed = new MessageEmbed()
+        .setTitle('content')
+        .setDescription(message.content)
+        .setColor(STARBOARD_EMBED_COLOR)
+        .setTimestamp(message.createdTimestamp)
+        .setAuthor(
+            'Starred Message',
+            message.author.displayAvatarURL({ dynamic: true })
+        )
+        .addFields(
+            { name: 'Author', value: message.author.toString(), inline: true },
+            {
+                name: 'Channel',
+                value: message.channel.toString(),
+                inline: true,
+            },
+            {
+                name: 'Jump To Message',
+                value: `[Click Me!](${message.url})`,
+                inline: false,
+            }
+        );
+    if (!reaction.emoji.id) {
+        embed.setFooter(
+            ` ${reaction.emoji.name} ${reaction.count} ${
+                reaction.emoji.name === DEFAULT_STARBOARD_EMOJI ? 'stars' : ''
+            }`
+        );
+    } else if (reaction.emoji.name && reaction.emoji.url) {
+        embed.setFooter(
+            `${reaction.count} ${reaction.emoji.name.toLowerCase()}`,
+            reaction.emoji.url
+        );
+    }
+    const firstAttachment = message.attachments.first();
+    if (firstAttachment) {
+        embed.setImage(firstAttachment.url);
+    }
+    return embed;
 }
