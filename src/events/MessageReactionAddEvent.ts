b7d1ee41f8b8175b12c1dfd00668395bc08f519d
Hidoni <ron30086@walla.com>
2021-09-25 22:26:49 +0300

    Finished handling messageReactionAdd event

--- src/events/MessageReactionAddEvent.ts
+++ src/events/MessageReactionAddEvent.ts
@@ -1,6 +1,122 @@
-import { MessageReaction, PartialMessageReaction, User } from 'discord.js';
+import {
+    GuildEmoji,
+    MessageReaction,
+    PartialMessageReaction,
+    ReactionEmoji,
+    TextBasedChannel,
+    User,
+    TextBasedChannels,
+} from 'discord.js';
 import { Bot } from '../client/Client';
 import { EventHandler } from '../interfaces/Event';
+import { GuildConfigInstance } from '../interfaces/GuildConfig';
+import { StarredMessageInstance } from '../interfaces/StarredMessages';
+import {
+    generateStarboardEmbed,
+    findStarboardChannelForTextChannel,
+} from '../utils/StarboardUtils';
+
+function isStarEmoji(
+    guildConfig: GuildConfigInstance,
+    emoji: GuildEmoji | ReactionEmoji
+) {
+    if (!emoji.id && guildConfig.isUnicode) {
+        return guildConfig.emoji === emoji.name;
+    }
+    return guildConfig.emoji === emoji.id;
+}
+
+async function getStarboardChannelFromChannel(
+    config: GuildConfigInstance,
+    channel: TextBasedChannels,
+    client: Bot
+): Promise<TextBasedChannels | null> {
+    if (channel.type === 'DM') {
+        throw new Error('Invalid channel for starred message');
+    }
+    const channelId = await findStarboardChannelForTextChannel(
+        config,
+        channel,
+        client.database
+    );
+    const starboardChannel = channelId
+        ? await client.channels.fetch(channelId)
+        : null;
+    if (!starboardChannel) {
+        client.logger?.debug(
+            `Starboard channel could not be fetched (id is ${channelId}), assuming intentional`
+        );
+    } else if (!(starboardChannel instanceof TextBasedChannel)) {
+        client.logger?.debug(
+            `Misconfigured starboard channel (id is ${channelId}) isn't text-based`
+        );
+        return null;
+    }
+    return starboardChannel as TextBasedChannels;
+}
+
+async function starPublicMessage(
+    client: Bot,
+    config: GuildConfigInstance,
+    reaction: MessageReaction
+) {
+    const starredMessage = await client.database.getStarredMessage(
+        reaction.message.id
+    );
+    if (starredMessage) {
+        await updateExistingStarredMessage(
+            config,
+            reaction,
+            client,
+            starredMessage
+        );
+    } else {
+        await createNewStarredMessage(reaction, config, client);
+    }
+}
+
+async function updateExistingStarredMessage(
+    config: GuildConfigInstance,
+    reaction: MessageReaction,
+    client: Bot,
+    starredMessage: StarredMessageInstance
+) {
+    const starboardChannel = await getStarboardChannelFromChannel(
+        config,
+        reaction.message.channel,
+        client
+    );
+    if (starboardChannel) {
+        let starboardMessage = await starboardChannel.messages.fetch(
+            starredMessage.starboardMessageId
+        );
+        await starboardMessage.edit({
+            embeds: [await generateStarboardEmbed(reaction)],
+        });
+    }
+    await starredMessage.update('starCount', reaction.count);
+    client.logger?.info(
+        `Updated existing starred message for message with id ${starredMessage}, new star count is ${reaction.count}`
+    );
+}
+
+async function createNewStarredMessage(
+    reaction: MessageReaction,
+    config: GuildConfigInstance,
+    client: Bot
+) {
+    const starboardChannel = await getStarboardChannelFromChannel(
+        config,
+        reaction.message.channel,
+        client
+    );
+    if (starboardChannel) {
+        let starboardMessage = await (
+            starboardChannel as TextBasedChannels
+        ).send({ embeds: [await generateStarboardEmbed(reaction)] });
+        await client.database.addNewStarredMessage(reaction, starboardMessage);
+    }
+}
 
 export const name: string = 'messageReactionAdd';
 export const handler: EventHandler = async (
@@ -11,7 +127,17 @@ export const handler: EventHandler = async (
     if (reaction.partial) {
         reaction = await reaction.fetch();
     }
-    if (reaction.message.author == user) {
-        await reaction.users.remove(user);
+    if (reaction.message.guildId) {
+        const guildConfig: GuildConfigInstance =
+            await client.database.getGuildConfig(reaction.message.guildId);
+        if (isStarEmoji(guildConfig, reaction.emoji)) {
+            if (reaction.message.author == user) {
+                await reaction.users.remove(user);
+            } else {
+                if (reaction.count >= guildConfig.minimumReacts) {
+                    await starPublicMessage(client, guildConfig, reaction);
+                }
+            }
+        }
     }
 };
