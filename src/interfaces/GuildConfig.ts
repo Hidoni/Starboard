09da0661a6e6d4b50011417b8bc2e3105af228ae
Hidoni <ron30086@walla.com>
2021-09-25 22:20:12 +0300

    Fixed GuildConfig to represent null channel IDs

--- src/interfaces/GuildConfig.ts
+++ src/interfaces/GuildConfig.ts
@@ -3,8 +3,8 @@ import { Model } from "sequelize/types";
 
 export interface GuildConfigInstance extends Model {
     guildId: Snowflake;
-    sfwChannelId: Snowflake;
-    nsfwChannelId: Snowflake;
+    sfwChannelId: Snowflake | null;
+    nsfwChannelId: Snowflake | null;
     emoji: Snowflake | string;
     isUnicode: boolean;
     minimumReacts: number;
